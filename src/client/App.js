2e5882030d8771c120475e54b36de7a1ca59d0bb
Nritya Kamath <nritya.k@gmail.com>
2019-09-25 22:06:58 -0700

    Updated with more boilerplate code

--- src/client/App.js
+++ src/client/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Landing from './components/Landing';
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
-import ReactImage from './react.png';
+
 
 export default class App extends Component {
-  state = { username: null };
-
   componentDidMount() {
   }
 
   render() {
-    const { username } = this.state;
     return (
-      <div>
-        <h1>Welcome to Forage!</h1>
-        <img src={ReactImage} alt="react" />
-      </div>
+      <Container id="app-container">
+        <Nav>
+            <Link to='/'>Landing</Link>
+        </Nav>
+        <Row>
+          <h1>Forage</h1>
+        </Row>
+        <main>
+          <Route exact path="/" component={Landing} />
+        </main>
+      </Container>
     );
   }
 }
